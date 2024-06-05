99319ca61d09dba9277ae9cfc4d6a82330328fa5
saravanankandasamyGKS <gksaravanan008@gmail.com>
2024-06-05 14:14:02 +0530

    update

---
+++ Pettycash-Manager-Backend-main/index.js
@@ -0,0 +1,36 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import { client } from "./db/db.js";
+import { SigninRouter } from "./routes/login.js";
+import { expenseRouter } from "./routes/expense.js";
+import { IncomeRouter } from "./routes/income.js";
+
+//config for env 
+dotenv.config()
+// server Started 
+const app=express()
+const port=process.env.port;
+
+//database Connection Check
+client()
+
+//Middleware
+app.use(express.json())
+app.use(cors())
+app.use(cookieParser())
+
+
+// application routes
+app.use('/',SigninRouter)
+app.use('/data',expenseRouter)
+app.use('/input',IncomeRouter)
+
+
+
+
+// listening Server
+app.listen(port,() => {
+    console.log(`Server Started in localhost:${port}`);
+  })
